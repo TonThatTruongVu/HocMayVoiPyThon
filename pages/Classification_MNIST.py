f8b611ba22e171dd39489d17da0173f5623a13ec
TonThatTruongVu <106595074+TonThatTruongVu@users.noreply.github.com>
2025-03-03 05:25:44 +0700

    Update Buoi 4 and app_main.py

--- pages/Classification_MNIST.py
+++ pages/Classification_MNIST.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from Buoi4.Classification import Classification
+from Buoi4.Classification import main
 
 st.title("🔢 Classification MNIST")
 
 
-# Gọi hàm Classification từ module
-Classification()
+# Gọi hàm Classification từ modul
+main()
+    
